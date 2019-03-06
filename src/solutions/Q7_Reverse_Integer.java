6c2be85145bd637140d160b1780502381edaad68
Chand25252 <337226087@qq.com>
2019-03-06 22:23:46 +0800

    Solution for question 7.

---
+++ src/solutions/Q7_Reverse_Integer.java
@@ -0,0 +1,28 @@
+package solutions;
+
+/*给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。*/
+/**
+ * @author Chand
+ * @date 2019-03-06 22:21:31
+ */
+public class Q7_Reverse_Integer {
+    public int reverse(int x) {
+        if(x == Integer.MIN_VALUE) {
+            return 0;
+        }
+        if(x < 0) {
+            return -reverse(-x);
+        }
+        int temp = x;
+        Long newNum = 0L;
+        while(temp > 0) {
+            newNum = newNum * 10;
+            newNum = newNum + temp % 10;
+            temp = temp / 10;
+        }
+        if(newNum > Integer.MAX_VALUE) {
+            newNum = 0L;
+        }
+        return newNum.intValue();
+    }
+}
