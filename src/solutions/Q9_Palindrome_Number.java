98efc7d35db509b8f0a93a57338a0cef402c34d2
Chand25252 <337226087@qq.com>
2019-03-08 22:15:39 +0800

    Solution for question 9.

---
+++ src/solutions/Q9_Palindrome_Number.java
@@ -0,0 +1,25 @@
+package solutions;
+
+/*
+    判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+*/
+
+/**
+ * @author Chand
+ * @date 2019-03-08 22:13:47
+ */
+public class Q9_Palindrome_Number {
+    public boolean isPalindrome(int x) {
+        if(x < 0) {
+            return false;
+        }
+        int temp = x;
+        int result = 0;
+        while(temp > 0) {
+            result = result * 10;
+            result = result + temp % 10;
+            temp = temp / 10;
+        }
+        return result == x;
+    }
+}
